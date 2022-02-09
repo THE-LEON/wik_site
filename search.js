69158bdc5db68f5dad798ee953e50757b6ed2d21
THE-LEON <78154572+THE-LEON@users.noreply.github.com>
2022-02-09 22:21:52 +0300

    Add files via upload

---
+++ search.js
@@ -0,0 +1,32 @@
+function search_k(elem){
+    if(event.key === 'Enter') {
+        search(elem.value);        
+    }
+
+}
+function search_z(){
+    search(document.getElementById("search").value);
+}
+
+function search(val){
+    
+    let arr;
+    let res_str = [];
+    let array = fetch('./data.json').then((resp) => {return resp.json();}).then((data) => {try{
+        data = JSON.parse(desif(JSON.stringify(data)));
+        let x = 0;
+        for(x;x < data.length;x++){
+            for(let j = 0;j < data[x].tags.length;j++){
+                if(val.toLowerCase().match(data[x].tags[j])){
+                    res_str.push(x);
+                }}}
+            let url = new URL(`${location.origin}/sub_pages/search.html`);
+            url.searchParams.set("s",res_str.join("-"));
+
+            console.log(url)
+            location.href = url;}catch(err){
+        document.getElementById("recode").innerText = "ввести пароль ещё раз";
+    }
+        
+        });}
+            
